f2150bc3ba04b7251832a99663f2383bc8e142eb
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-04-05 15:18:28 +0300

    bottom bar

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem->
+           when(menuItem.itemId){
+               R.id.bottom_home->{
+                   replaceFragment(HomeFragment())
+                   true
+               }
+               R.id.bottom_profile->{
+                   replaceFragment(ProfileFragment())
+                   true
+               }
+               R.id.bottom_settings->{
+                   replaceFragment(SettingsFragment())
+                   true
+               }
+               else->false
+           }
+        }
+        replaceFragment(HomeFragment())
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container,fragment).commit()
     }
 }
